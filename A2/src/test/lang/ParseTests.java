9a6ccd09e8902ca6718d213618e54d4c26097c48
Ishaan Nagpal <inagpal@ucsb.edu>
2023-09-08 14:57:03 +0200

    in - Step 1 Completed

--- A2/src/test/lang/ParseTests.java
+++ A2/src/test/lang/ParseTests.java
@@ -8,9 +8,6 @@ public class ParseTests {
 	/** Directory where the test input files are stored. */
 	private static final File TEST_DIRECTORY = new File("testfiles/parser");
 
-	@Test public void example() {
-		Util.testValidSyntax(TEST_DIRECTORY, "example.in");
-	}
 
 	@Test
 	public void error() {
