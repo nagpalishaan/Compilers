ac7744de421163e3704f56bbca903d0daa9dd66b
Brenna <brennascholte@ucsb.edu>
2023-09-14 09:44:51 +0200

    bs - added example2.in to test parser

--- A2/src/test/lang/ParseTests.java
+++ A2/src/test/lang/ParseTests.java
@@ -8,6 +8,10 @@ public class ParseTests {
 	/** Directory where the test input files are stored. */
 	private static final File TEST_DIRECTORY = new File("testfiles/parser");
 
+	@Test
+	public void example2() {
+		Util.testValidSyntax(TEST_DIRECTORY, "example2.in");
+	}
 
 	@Test
 	public void error() {
